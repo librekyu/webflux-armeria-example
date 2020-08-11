5475e8a782e9f50e047e17744d714f66e672fe7d
김홍규 <khghong1@gmail.com>
2020-08-11 17:08:25 +0900

    [#2] annotation 흐름 및 프로젝트 구조 파악

--- src/main/java/com/bluedawn/sample/controller/DemoController.java
+++ src/main/java/com/bluedawn/sample/controller/DemoController.java
@@ -3,12 +3,14 @@ package com.bluedawn.sample.controller;
 import com.bluedawn.sample.model.DemoModel;
 import com.bluedawn.sample.service.DemoService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Controller
 @AllArgsConstructor
 public class DemoController {
@@ -20,6 +22,7 @@ public class DemoController {
     public Mono<ResponseEntity<DemoModel>> getRandom() {
 
         return demoService.getRandomDemoModel().map(demoModel -> {
+            log.info("demoModel > " + demoModel.getArgs().toString());
 
             if (demoModel.getArgs().get("randomNumber").equals("0")) {
                 return ResponseEntity.noContent().build();
--- src/main/java/com/bluedawn/sample/core/Personal.java
+++
@@ -1,9 +0,0 @@
-package com.bluedawn.sample.core;
-
-import java.util.List;
-
-public interface Personal {
-    void init();
-    Person findByName(String name);
-    List<Person> findAll();
-}
